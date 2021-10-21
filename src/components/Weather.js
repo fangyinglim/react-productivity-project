1c90a8f016da7db9b122319020433421bc81d3ab
fangyinglim <limfy97@gmail.com>
2021-10-21 12:08:02 +0800

    added custom hook and to fix date timezone format

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "./useFetch";
 import "./Weather.css";
 
 function Weather() {
-  let todayDate = new Date().toISOString().slice(0, 19).replace(/-/g, "/");
+  let todayDate = new Date().toISOString().slice(0, 19).replace(/:/g, "%3A");
   console.log(todayDate);
+
+  let url = `https://api.data.gov.sg/v1/environment/air-temperature?date_time=${todayDate}`;
+  console.log(url);
+
+  let weather = useFetch(url);
+  console.log(weather);
+
+  const weatherRefreshHandle = () => {
+    // console.log(weather);
+    // return weather;
+  };
 
   return (
     <div className="weather-container">
       <header>Weather</header>
+      <button onClick={weatherRefreshHandle}>🔄</button>
+      <div>{parseInt(weather).toFixed(1)}</div>
     </div>
   );
 }
--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,5 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const useFetch = (url) => {};
+const useFetch = (url) => {
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    fetch(url).then((res) =>
+      res.json().then((data) => {
+        console.log(data.items[0].readings);
+        let totalTemp = data.items[0].readings
+          .map((data) => data.value)
+          .reduce(function (previous, current) {
+            return previous + current;
+          });
+        console.log(totalTemp);
+        let avgTemp = totalTemp / data.items[0].readings.length;
+        console.log(avgTemp);
+        setWeather(avgTemp);
+      })
+    );
+  }, [url]);
+
+  return weather;
+};
 
 export default useFetch;
